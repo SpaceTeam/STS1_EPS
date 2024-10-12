2f7f0c2d60b10a83120f11ea3d8951cb98012708
David Freismuth <davfreismuth@gmail.com>
2024-10-12 15:11:05 +0200

    Changed net labels of the panel connectors, so they are consistent with the name of the respective connector.

--- scripts/replacer.py
+++ scripts/replacer.py
@@ -2,15 +2,15 @@ import argparse
 import os
 
 REPLACE_MAP = {
-    "Panel_Y-_": "Panel_Y+_",
-    "Panel_Y+_": "Panel_Y-_",
-    "Panel_X-_": "Panel_Z+_",
-    "Panel_Z-_": "Panel_X+_",
-    "Panel_X+_": "Panel_Z-_"
+    "_Y-_": "_Y+_",
+    "_Y+_": "_Y-_",
+    "_X-_": "_Z+_",
+    "_Z-_": "_X+_",
+    "_X+_": "_Z-_"
 }
 
 
-def main(file_names, in_place):
+def main(file_names, in_place, silent):
     for file_name in file_names:
         with open(file_name, 'r') as f:
             lines = f.readlines()
@@ -27,25 +27,38 @@ def main(file_names, in_place):
             to_str = replace_position["to"]
             lines_modified[at] = lines_modified[at].replace(from_str, to_str)
 
+        target_file_name = ""
         if in_place:
-            with open(file_name, 'w') as target:
-                target.writelines(lines_modified)
+            target_file_name = file_name
 
         else:
             file_name_no_ext, file_ext = os.path.splitext(file_name)
             file_base_name = os.path.basename(file_name_no_ext)
             new_file_name = os.path.join(os.path.dirname(file_name),
                                          file_base_name + "_modified" + file_ext)
+            target_file_name = new_file_name
 
-            with open(new_file_name, 'w') as target:
-                target.writelines(lines_modified)
+        with open(target_file_name, 'w') as target:
+            target.writelines(lines_modified)
+            if not silent:
+                print("Went through {} lines and replaced {} occurences in {}.".
+                    format(
+                        len(lines_modified),
+                        len(replace_positions),
+                        os.path.basename(target_file_name)
+                    )
+                )
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file_names", nargs='+')
     parser.add_argument("--in_place", action='store_true')
+    parser.add_argument("--silent", action='store_true')
 
     args = parser.parse_args()
 
-    main(args.file_names, args.in_place)
+    main(args.file_names, args.in_place, args.silent)
+    if not args.silent:
+        print("Done.")
